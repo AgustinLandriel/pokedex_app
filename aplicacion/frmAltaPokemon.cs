d5c809fddf670936f7c87b1c49fdf3ecc7aabc3a
Agustin Landriel <agusrojo20@hotmail.com>
2022-12-02 11:06:47 -0300

    Se agrega modificar pokemones

---
+++ aplicacion/frmAltaPokemon.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dominio;
+using controlador;
+
+namespace aplicacion
+{
+    public partial class frmAltaPokemon : Form
+    {
+        
+        AccesoDatos datos = new AccesoDatos();
+        PokemonNegocio pokemonNegocio = new PokemonNegocio();
+        Pokemon pokemon = null;
+
+        public frmAltaPokemon()
+        {
+            InitializeComponent();
+        }
+        public frmAltaPokemon(Pokemon pokemon)
+        {//Sobreescribo el constructor que recibe un pokemon a modificar
+            InitializeComponent();
+            this.pokemon = pokemon;
+            Text = "Modificar Pokemon";
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {   //Si el pokemon no existe,lo crea
+                if(pokemon == null)
+                   pokemon = new Pokemon();
+                
+                //Si ya existe,sobreescribe los datos
+                pokemon.Numero = int.Parse(textNumero.Text);
+                pokemon.Nombre = textNombre.Text;
+                pokemon.Descripcion = textDescripcion.Text;
+                pokemon.Tipo = (Elemento)comboBoxTipo.SelectedItem;
+                pokemon.Debilidad = (Elemento)comboBoxDebilidad.SelectedItem;
+
+                if(pokemon.Id != 0)
+                {
+                    pokemonNegocio.modificarPokemon(pokemon);
+                    MessageBox.Show("Modificado correctamente");
+                    
+                }
+                else
+                {
+                    pokemonNegocio.agregarPokemon(pokemon);
+                    MessageBox.Show("Agregado correctamente");
+                    
+                }
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void frmAltaPokemon_Load(object sender, EventArgs e)
+        {
+            ElementoNegocio elementoNegocio = new ElementoNegocio();
+            
+            comboBoxTipo.DataSource = elementoNegocio.getElementos();
+            comboBoxTipo.ValueMember = "IdElemento";
+            comboBoxTipo.DisplayMember = "Descripcion";
+            comboBoxDebilidad.DataSource = elementoNegocio.getElementos();
+            comboBoxDebilidad.ValueMember = "IdElemento";
+            comboBoxDebilidad.DisplayMember = "Descripcion";
+
+
+            if (pokemon != null)
+            {
+                textNumero.Text = pokemon.Numero.ToString();
+                textNombre.Text = pokemon.Nombre;
+                textDescripcion.Text = pokemon.Descripcion;
+                textUrlImagen.Text = pokemon.UrlImagen;
+                cargarImagen(pokemon.UrlImagen);
+                comboBoxTipo.SelectedValue = pokemon.Tipo.IdElemento;
+                comboBoxDebilidad.SelectedValue = pokemon.Debilidad.IdElemento;
+            }
+        }
+
+        //Cuando salgo del input de la URL me carga la imagen
+        private void textUrlImagen_Leave(object sender, EventArgs e)
+        {
+            cargarImagen(textUrlImagen.Text);
+        }
+        void cargarImagen(string urlImagen)
+        {
+            try
+            {
+                pictureBoxPokemon.Load(urlImagen);
+            }
+            catch (Exception)
+            {
+
+                pictureBoxPokemon.Load("https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg");
+            }
+        }
+    }
+}
---
+++ controlador/PokemonNegocio.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace controlador
+{
+    public class PokemonNegocio
+    {
+        AccesoDatos datos = new AccesoDatos();
+        List<Pokemon> listaPokemon = new List<Pokemon>();
+
+        public List<Pokemon> getPokemon()
+        {
+           
+
+            try
+            {
+                datos.setQuery(@"SELECT p.id,numero,nombre,p.descripcion,urlImagen,e.descripcion as Tipo, d.descripcion as Debilidad,idTipo,IdDebilidad 
+                                 from POKEMONS as p
+                                 inner join ELEMENTOS e 
+                                 on ( p.IdTipo = e.Id)
+                                 inner join elementos d 
+                                 on(p.IdDebilidad = d.Id)");
+
+                datos.abrirConexion();
+
+                while (datos.Lector.Read())
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Id = (int)datos.Lector["id"];
+                    aux.Numero = (int)datos.Lector["numero"];
+                    aux.Nombre = (string)datos.Lector["nombre"];
+                    aux.Descripcion = (string)datos.Lector["descripcion"];
+
+                    //Si los datos son nulos no los leo
+                    if(!(datos.Lector["urlImagen"] is DBNull))
+                    aux.UrlImagen = (string)datos.Lector["urlImagen"];
+                    aux.Tipo = new Elemento();
+                    aux.Tipo.IdElemento = (int)datos.Lector["IdTipo"];
+                    aux.Tipo.Descripcion = (string)datos.Lector["Tipo"];
+                    aux.Debilidad = new Elemento();
+                    aux.Debilidad.IdElemento = (int)datos.Lector["IdDebilidad"];
+                    aux.Debilidad.Descripcion = (string) datos.Lector["Debilidad"];
+
+                    listaPokemon.Add(aux);
+                }
+
+                return listaPokemon;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void agregarPokemon(Pokemon pokemon)
+        {
+
+            try
+            {
+                datos.setQuery("INSERT INTO POKEMONS (numero,nombre,descripcion,idTipo,IdDebilidad) values (" + pokemon.Numero + ",'" + pokemon.Nombre + "','" + pokemon.Descripcion + "',@IdTipo,@IdDebilidad)");
+                datos.setVariables("@IdTipo", pokemon.Tipo.IdElemento);
+                datos.setVariables("@IdDebilidad", pokemon.Debilidad.IdElemento);
+                datos.commitConsulta();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificarPokemon(Pokemon pokemon)
+        {
+            try
+            {
+                datos.setQuery("UPDATE POKEMONS set numero = @numero,nombre=@nombre,descripcion = @descripcion,urlImagen = @urlimagen,IdTipo = @IdTipo,IdDebilidad=@IdDebilidad WHERE id = @id");
+                datos.setVariables("@numero", pokemon.Numero);
+                datos.setVariables("@nombre", pokemon.Nombre);
+                datos.setVariables("@descripcion", pokemon.Descripcion);
+                datos.setVariables("@urlimagen", pokemon.UrlImagen);
+                datos.setVariables("@IdTipo", pokemon.Tipo.IdElemento);
+                datos.setVariables("@IdDebilidad", pokemon.Debilidad.IdElemento);
+                datos.setVariables("@Id", pokemon.Id);
+                datos.commitConsulta();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+    }
+}
